5b9740e58fa58efa4d1ca6c60776622a8bca90cb
sausagemountain <miha.vanya@gmail.com>
2019-10-13 22:44:38 +0500

    Создана пустая база данных

---
+++ Library/MyDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Common;
+using System.Data.Entity;
+using System.Data.SQLite;
+
+namespace Library
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(string nameOrConnectionString) : base(new SQLiteConnection(nameOrConnectionString), true)
+        {
+            Database.CreateIfNotExists();
+        }
+    }
+}
